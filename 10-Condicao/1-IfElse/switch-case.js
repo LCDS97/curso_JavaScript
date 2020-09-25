a0d6ee3da7ae2fd6b89cf8eb0043cc11cb7e676e
LCDS97 <lucas.cds1997@gmail.com>
2020-09-25 00:45:16 -0300

    Terminei Condições
    Terminado módulo de condição
    Iniciado modulo de laços

--- 10-Condicao/1-IfElse/switch-case.js
+++ 10-Condicao/1-IfElse/switch-case.js
@@ -1,11 +1,26 @@
-let permissao; // Usuário, Gerente, Diretor
+
+// Usuário, Gerente, Diretor
 
 // Switch - Case faz voce trabalhar com diversas situações onde se acontece aquela determinada condição é aplicado um resultado
 // ou seja você ja sabe os valores das condição e aplica o caso para aquela condição
-
-switch (permissao) {
+function mostrarUsuario(){
+    valor = document.getElementById("user").value; 
+switch (valor) {
     case 'comum':
-        console.log('Usuário Comum');
+        document.getElementById("resultado").innerHTML = "Seu tipo de usuário é o comum"
     // break serve para ele para a condição aqui e não comparar os outros resultados do case
     break;
+
+    case 'gerente':
+        document.getElementById("resultado").innerHTML = "Seu tipo de usuário é o gerencia"
+    break;
+
+    case 'diretor':
+        document.getElementById("resultado").innerHTML = "Seu tipo de usuário é o diretor"
+    break;
+
+    // Caso não cai em nenhuma das 3 condições, aplicamos o Default
+    default:
+        document.getElementById("resultado").innerHTML = "Seu tipo de usuário esta incorreto! Tente Novamente"
+}
 }
