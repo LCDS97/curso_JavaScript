a816ec74b4704e02b4936d685efb04147d99bd43
LCDS97 <lucas.cds1997@gmail.com>
2020-09-26 23:15:44 -0300

    Projeto feito: Maior entre valores
    Projeto feito com uma função de valorMaior e valorMenor para comparação de maior e menor número entre dois números usado pela função
    Primeira função com comparador
    Segunda função com operador ternario

---
+++ Projeto/2-MaximoEntreDoisValores/maior-valor.js
@@ -0,0 +1,17 @@
+// Função para usar 2 numeros e retorna o maior valor entre eles
+
+// Definindo valores
+let valorMaior = max(5,10);
+console.log(valorMaior);
+// Função para comparar o número maior
+function max(numero1,numero2){
+    if(numero1 > numero2)
+    return numero1;
+    else return numero2;
+}
+// Com operador ternário
+let valorMenor = min(15,34)
+console.log(valorMenor)
+function min(numero3,numero4) {
+    return numero3 < numero4 ? numero3: numero4;
+}
