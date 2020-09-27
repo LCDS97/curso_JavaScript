dafacb51c0b5d29cfa0130257a02137a4e39ec80
LCDS97 <lucas.cds1997@gmail.com>
2020-09-27 03:39:36 -0300

    Iniciado Mini-Projeto - FizzBuzz
    Comecei Mini Projeto de FizzBuzz com base em divisivel de 3 e 5

---
+++ Projeto/3-FizzBuzz/fizzbuzz.js
@@ -0,0 +1,12 @@
+//FizBuzz - Lista dos seguintes casos:
+// Divisivel por 3 => ira retornar Fizz
+// Divisivel por 5 => ira retornar Buzz
+// Divisivel por 3 e 5 => ira retornar FizzBuzz
+// Não divisivel por 3 ou 5 => entrada
+// Não é um número => Mensagem de erro
+const resultado = fizzBuzz(3);
+console.log(resultado);
+
+function fizzBuzz(entrada) {
+
+}
