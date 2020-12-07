a5f3f3b41507e6fdd0c43575860be78e06d0e993
LCDS97 <lucas.cds1997@gmail.com>
2020-12-07 00:48:54 -0300

    Completado Template Literal
    Aprendendo conceito do Template Literal dentro do JS, apliquei em mensagem personalizada

---
+++ 18-TemplateLiteral/templateLiteral.js
@@ -0,0 +1,25 @@
+const mensagem = "Oi isso é a minha primeira mensagem";
+
+// Tipos de literais
+// Object = {}
+// Boolean = True or False
+// String = "" or ''
+// Template ``
+
+const outra = `Oi isso é uma
+mensagem 'quebradinha'`;
+
+console.log(outra)
+
+// Template (``), mantem a formatação da imagem
+// Caso queira indicar uma variavel para uma mensagem dinamica, utiliza-se {} e o $ para definir a variavel
+
+const nome = "Gerundio da Silva Santos";
+
+const email =
+`Olá, sr.(a) ${nome}
+
+O senhor hoje ta ruim, toma um litrão porque hoje to contente
+
+Valeu,
+Deivison dos Santos`;
